ed6d59f8d9a2ea82cb38f7b85783dd27e6da6711
NimrakMill <49028294+NimrakMill@users.noreply.github.com>
2019-03-30 01:28:24 +0700

    Add files via upload

---
+++ NSISv2.sh
@@ -0,0 +1,340 @@
+
+"=============================================================="
+clear
+
+figlet -f slant Installler Tools
+
+echo "========================================" | lolcat
+echo "Tools By NimrakMill" | lolcat
+echo "Tools Installer" | lolcat
+echo "Team : CYBER N.S.I.S (Nsis)" | lolcat
+echo "========================================" | lolcat
+echo
+echo ""
+echo ""
+echo "#       #     #######     ######### $yellow " | lolcat
+echo "#       #     #           # $red " | lolcat
+echo "#########     #           # $green " | lolcat
+echo "    #         ######      ########### $yellow " | lolcat
+echo "    #         #                     # $green " | lolcat
+echo "    #         #######     ############ $red " | lolcat
+echo ""
+echo "1!1!1!1!1!1!1!1..Kontoeeeeeeeell......$green " | lolcat
+echo ""
+echo "#     #     #     #####         ######### $red " | lolcat
+echo "#     # #   #     #    #       #         # $red " | lolcat
+echo "#     #  #  #     #     #      #         # $red " | lolcat
+echo "#     #   # #     #      #     #         # $red " | lolcat
+echo "#     #    ##     #     #      #         # $red " | lolcat
+echo "#     #     #     ######        ######### $red " | lolcat
+echo ""
+echo "========================================================="
+echo "1. Installer BAJINGAN $red " | lolcat
+echo "=================="
+echo "2.Installer TU4N G4L4U $green " | lolcat
+echo "=================="
+echo "3.Install Spam $yellow " | lolcat
+echo "=================="
+echo "4.Installer CrewBot $orange " | lolcat
+echo "=================="
+echo "5.Install MBF $red " | lolcat
+echo "=================="
+echo "6.Hack Cctv $green " | lolcat
+echo "=================="
+echo "7.Installer TUANB4DUT $yellow " | lolcat
+echo "=================="
+echo "8.Hack instagram $orange " | lolcat
+echo "=================="
+echo "9.Tembax Xl $red " | lolcat
+echo "=================="
+echo "10.Tes Kecepatan Jaringan $green " | lolcat
+echo "=================="
+echo "11.Hack FB DLL $yellow " | lolcat
+echo "=================="
+echo "12.Spamm WA $orange " | lolcat
+echo "=================="
+echo "13.200+ Tools $red " | lolcat
+echo "=================="
+echo "14.Install OSIF $green " | lolcat
+echo "=================="
+echo "15.Hack WA $yellow " | lolcat
+echo "=================="
+echo "16.Install LazyGo $green " | lolcat
+echo "=================="
+echo "17.Bukan-Untuk-Umum $yellow " | lolcat
+echo "=================="
+echo "18.Install FakeCall $red " | lolcat
+echo "=================="
+echo "19.Install IslamPray $orange " | lolcat
+echo "=================="
+echo "20.Install ARTEMATIKA $yellow " | lolcat
+echo "========================================================="
+read -p "r00t@NimrakMill~# " pill
+if [ $pill = 1 ]
+then
+clear
+echo "Tungguu Kontoeell....1!1!1"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/DarknessCyberTeam/...
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+
+if [ $pill = 2 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/TU4NG4L4U/InstallerV6
+cd InstallerV6
+sh InstallerV6.sh
+fi
+
+
+if [ $pill =3 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/mrmsdv/sdvspam.git
+cd sdvspam
+sh install.sh
+fi
+
+if [ $pill =4 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/Xeit666h05t/CrewBot
+ls
+cd CrewBot
+python2 CrewBot.pyc
+fi
+
+if [ $pill =5 ]
+then
+clear
+echo "Tunggu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/pirmansx/mbf
+cd mbf
+python2 MBF.py
+fi
+
+
+if [ $pill =6 ]
+then
+clear
+echo "Tunguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+pip2 install requests
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+
+if [ $pill =7 ]
+then
+clear
+echo "Tunggu Kontoeell.....1!1!1"
+echo
+sleep 1
+apt install git
+apt install php
+apt install curl
+apt install ruby
+apt install figlet
+apt install python2
+gem install lolcat
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+chmod +x TUANB4DUT.sh
+sh TUANB4DUT.sh
+fi
+
+if [ $pill =8 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install nano
+pkg install git
+pkg install nano
+git clone https://github.com/avramit/instahack.git
+ls
+cd instahack
+ls
+pip install requests
+cd instahack
+nano pass.txt
+cat pass.txt
+ls
+python hackinsta.py
+fi
+
+if [ $pill =9 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+pkg install python
+git clone https://github.com/albertoanggi/xl-py
+pip install -r requirements.txt
+chmod +x app.py
+python2 app.py
+fi
+
+if [ $pill =11 ]
+then
+clear
+echo "Tunguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+pkg ins toilet
+pkg ins figlet
+pkg ins ruby
+pkg ins gem
+gem ins lolcat
+pkg ins php
+pkg ins python2
+pkg ins git
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh v2.sh
+fi
+
+fi [ $pill =12 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install php
+pkg install git -y
+git clone https://github.com/siputra12/prank
+cd prank
+php wa.php
+fi
+
+if [ $pill =13 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+apt install bash
+apt install git
+git clone https://github.com/21D4N404/tools-installer.git
+cd tools-installer
+sh install.sh
+fi
+
+if [ $pill =14 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+apt install python2
+pip2 install requests mechanize
+apt install git
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pill =15 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+pkg install bash
+git clone https://github.com/AndriGanz/whatshack
+cd whatshack
+bash whatshack.sh
+fi
+
+if [ $pill =16 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/kadal15/LazyGo
+cd LazyGo
+sh install.sh
+fi
+
+if [ $pill =17 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/kadal15/bukan-untuk-umum
+cd bukan-untuk-umum
+sh M7M-installer.sh
+fi
+
+if [ $pill =18 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+pkg install php
+git clone https://github.com/FajarTheGGman/FakeCall
+cd FakeCall
+php spam.php
+fi
+
+if [ $pill =19 ]
+then
+clear
+echo "Tungguu Kontoell.....1!1!1"
+echo
+sleep 1
+pkg install git
+pkg install php
+git clone https://github.com/FajarTheGGman/IslamPray
+cd IslamPray
+php islam.php
+fi
+
+if [ $pill =20 ]
+then
+clear
+echo "Tungguu Kontoeell.....1!1!1"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/TUANB4DUT/ARITMATIKA
+cd ARTEMATIKA
+ls
+fi
+===============================================================
